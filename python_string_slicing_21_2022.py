1fd564dca44a0252caa59f48844d464cfee01f32
padatirdhapumadhu <102537051+padatirdhapumadhu@users.noreply.github.com>
2022-03-30 22:27:40 +0530

    python

---
+++ python_string_slicing_21_2022.py
@@ -0,0 +1,74 @@
+##var =[]
+##print(type(var))
+
+##var = ["a","b",2,4,5.6]
+##print(var)
+
+##var = ["a","b",2,4,5.6]
+##print(type(var))
+
+##var = ["a","b",2.4,5.6]
+##print(var[3])
+
+##var = ["a","b",2.4,5.6]
+##print(var[-3]) # negative index starts from -1
+
+##var = ["a","b",2,4,5.6] #it is used to replace
+##var[0] ="dhoni"
+##print(var)
+
+##var = ["a","B"]
+##var1 =[1,2]
+##print(var+var1)
+
+##var = ["a","b",2,4,5.6]
+##print(dir(var))
+
+##var =["a","b",2,4,5.6]
+##var.insert(0,"dhoni") #it is used to insert a comment
+##print(var)
+
+##var = ["a","b",2,4,5.6]
+##var.append("madhu")
+##print(var) #append takees only one argument
+
+##var = ["a","b",2,4,5.6]
+##var.append("madhu","ram")
+##print(var)
+
+##var = ["a","b",2,4,5.6]
+##var.extend(["madhu","ram"])
+##print(var)
+##
+##var = ["ant","india","bat","criocket","zoom"]
+##var.sort()
+##print(var) # is is used to alphabetical order
+
+##var = ["ant","india","bat","criocket","zoom"]
+##var.sort(reverse = True)
+##print(var)
+
+##var = ["ant","india","bat","criocket","zoom"]
+##output = sorted(var)
+##print(output) 
+           
+##var = "dhoni"
+##print(var[0])
+##print(var[-5])
+##
+####d - 0,-5
+####h - 1,-4
+####o - 2,-3
+####n - 3,-2
+####i - 4,-1
+##
+var = "dhoni is csk captain" #string slicing can t happen on reverse direction
+##print(var[0])
+##print(var[0:])
+##print(var[0:5])
+##print(var[6:2]) #this one is in reverse order
+##print(var[0:4])
+##print(var[2:3])
+##print(var[:-3])
+##print(var[0:-5])
+##print(var[::-3])
